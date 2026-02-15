a9a64614cf20b6c0f531e419b62c3585c99459b2
Maksim Butenko <lumosp55@gmail.com>
2026-02-15 19:41:59 +0300

    changing the parser form, adding selenium

--- src/parser.py
+++ src/parser.py
@@ -4,79 +4,86 @@ from pathlib import Path
 import time
 import json
 from loguru import logger
-from src.config import HEADERS, DELAY, OUTPUT_DIR
+from config import HEADERS, DELAY, OUTPUT_DIR
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-class Parser:
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.headers.update(HEADERS)
-        
-    def fetch_page(self, url):
-        try:
-            response = self.session.get(url, timeout=10)
-            response.raise_for_status()
-            time.sleep(DELAY)
-            return response.text
-        except Exception as e:
-            logger.error(f"Ошибка загрузки {url}: {e}")
-            return None
-        
-    def parse_page(self, url):
-        html = self.fetch_page(url)
-        if not html:
-            return None
-        soup = BeautifulSoup(html, 'html.parser')
-        
-        game_tile = soup.find_all(attrs={'data-qa': lambda x: x and 'productTile' in x})
-        
-        games = []
-        
-        for title in game_tile:
-            
-            game_data = {}
-            
-            name = title.find("span", class_="psw-t-body psw-c-t-1 psw-t-truncate-2 psw-m-b-2")
-            game_data['name'] = name.text.strip() if name else None
-            
-            price = title.find("span", class_="psw-m-r-3")
-            game_data['price'] = price.text.strip() if price else None
-            
-            if not game_data['name'] and not game_data['price']:
-                continue
 
-            if game_data['name'] in [game['name'] for game in games]:
-                continue
-            
-            games.append(game_data)
-            
-        return games
+url_main = "https://zara-russia.ru"
+        
+def get_soup(url):
+    try:
+        response = requests.get(url=url, timeout=5, headers=HEADERS)
+        time.sleep(DELAY)
+        logger.info(f"Успешно загружена страница: {url}")
+        return BeautifulSoup(response.text, 'lxml')
+    except Exception as e:
+        logger.error(f"Ошибка загрузки {url}: {e}")
+        return None
+    
+def parse_page(soup):
+    collections = soup.find_all('div', class_='t959__card t959__card_25')
+    
+    collections_list = []
+    
+    for collection in collections:
+        collection_dict = {
+            'title': collection.h2.text.strip(),
+            'url': f'{url_main}{collection.a.get('href')}'
+        }
+        
+        collections_list.append(collection_dict)
+        
+    return collections_list
+
+def save_csv(data):
+    keys = data[0].keys()
+    
+    Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+    filepath = Path(OUTPUT_DIR)
+    
+    with open(filepath / 'data.csv', 'w') as file:
+        dict_writer = csv.DictWriter(file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
+        
+def save_json(collections):
+    Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+    filepath = Path(OUTPUT_DIR)
+    
+    with open(filepath / 'collections.json', 'w') as file:
+        json.dump(collections, file, indent=4, ensure_ascii=False)
+    
+def selenium_parse_page(url):
+    
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(5)
+    
+    elements = driver.find_elements(By.CSS_SELECTOR, '.js-product.t-store__card.t-store__stretch-col.t-store__stretch-col_25.t-align_left.t-item')
+    
+    elem_list = []
+    
+    for element in elements:
+        element_dict = {
+            'title': element.find_element(By.CSS_SELECTOR, '.js-store-prod-name.js-product-name.t-store__card__title.t-typography__title.t-name.t-name_xs').text.strip(),
+            'url': element.find_element(By.TAG_NAME, 'a').get_attribute('href'),
+            'price': element.find_element(By.CSS_SELECTOR, '.js-product-price.js-store-prod-price-val.t-store__card__price-value').text.strip()
+        }
+        elem_list.append(element_dict)
+        
+    driver.quit()
+    
+    return elem_list
+    
+def main():
+    # soup = get_soup(url_main)
+    # collections = parse_page(soup)
+    # data = selenium_parse_page("https://zara-russia.ru/zhenshchiny_novinki_zara")
+    # save_csv(data)
+    # save_json(collections)
 
     
-    
-    def save_data(self, data, filename):
-        Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
-        
-        filepath = Path(OUTPUT_DIR) / filename
-        with open(filepath, 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-        
-        logger.info(f"Сохранено {len(data)} записей в {filepath}")
-    
-    def run(self, urls):
-        """Основной метод парсинга"""
-        results = []
-        for url in urls:
-            logger.info(f"Парсинг {url}")
-            data = self.parse_page(url)
-            if data:
-                results.extend(data)
-            else:
-                logger.warning(f"Не удалось спарсить {url}")
-        
-        if results:
-            self.save_data(results, 'parsed_data.json')
-        else:
-            logger.warning("Нет данных для сохранения")
-        
-        return results
-    
+if __name__ == "__main__":
+    main()
